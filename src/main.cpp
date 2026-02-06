72314e39f044c1efa7e6c467f1a00637028b9171
EDE-204 <duyenhuy410@gmail.com>
2026-02-06 08:36:15 +0700

    dht11

---
+++ src/main.cpp
@@ -0,0 +1,35 @@
+#include <Arduino.h>
+#include <DHT.h>
+
+#define DHTPIN 4      // Chân DATA nối với GPIO 4
+#define DHTTYPE DHT11 // Chọn loại cảm biến: DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("DHT11 Test");
+
+  dht.begin();
+}
+
+void loop() {
+  delay(2000); // DHT11 đọc tối đa ~1Hz
+
+  float humidity = dht.readHumidity();
+  float temperature = dht.readTemperature(); // Celsius
+  // float temperature = dht.readTemperature(true); // Fahrenheit
+
+  if (isnan(humidity) || isnan(temperature)) {
+    Serial.println("Khong doc duoc du lieu tu DHT11!");
+    return;
+  }
+
+  Serial.print("Do am: ");
+  Serial.print(humidity);
+  Serial.print(" %\t");
+
+  Serial.print("Nhiet do: ");
+  Serial.print(temperature);
+  Serial.println(" *C");
+}
